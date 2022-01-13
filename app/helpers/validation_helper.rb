267f30e7b675197b7b70d935c4c4b8c78809de37
Mick Zijdel <mickzijdel@hotmail.com>
2022-01-13 09:09:22 +0000

    Escape dash in regex

--- app/helpers/validation_helper.rb
+++ app/helpers/validation_helper.rb
@@ -1,5 +1,5 @@
 module ValidationHelper
   def self.filename_validation_regex
-    return { with: /\A[a-zA-Z0-9- ]*\z/, message: 'can only contain letters, numbers, spaces, and dashes' }
+    return { with: /\A[a-zA-Z0-9\- ]*\z/, message: 'can only contain letters, numbers, spaces, and dashes' }
   end
 end
